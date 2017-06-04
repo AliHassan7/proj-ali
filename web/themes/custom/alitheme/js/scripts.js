e696a88f4aadc35e3bb176ca5ce706936e4f63f9
Ali Hassan <ali.hassan@debugacademy.com>
2017-06-04 15:42:41 -0400

    Added social media icons to menu using js

--- web/themes/custom/alitheme/js/scripts.js
+++ web/themes/custom/alitheme/js/scripts.js
@@ -1,8 +1,15 @@
 (function ($) {
     Drupal.behaviors.myBehavior = {
         attach: function (context, settings) {
+            //Fade in effect for the main menu on desktop view
             $(".nav a").hide();
             $(".nav a").fadeIn(3500);
+            //Social media block at the bottom of the responsive menu
+            $('<div id="mobile-social"><ul>' +
+                '<li class="linkedin"><a href="https://www.linkedin.com/in/llalihassanll" target="_blank">LinkedIn</a></li>' +
+                '<li class="twitter"><a href="https://twitter.com/JustDrupaling" target="_blank">Twitter</a></li>' +
+                '<li class="instagram"><a href="https://www.instagram.com/loyaltyisfirst" target="_blank">Instagram</a></li>' +
+                '</ul></div>').insertAfter(".mm-listview");
         }
     };
 })(jQuery);
