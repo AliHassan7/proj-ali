f6a6d371cfdc5300136b38e3178a00c1817e8ee0
Ali Hassan <ali.hassan@debugacademy.com>
2017-06-10 19:30:54 -0400

    Styling view footer button and added mac images

--- web/themes/custom/alitheme/js/scripts.js
+++ web/themes/custom/alitheme/js/scripts.js
@@ -2,8 +2,8 @@
     Drupal.behaviors.myBehavior = {
         attach: function (context, settings) {
             //Fade in effect for the main menu on desktop view
-            $(".nav a").hide();
-            $(".nav a").fadeIn(3500);
+            $("#block-alitheme-main-menu-menu .nav a").hide();
+            $("#block-alitheme-main-menu-menu .nav a").fadeIn(3500);
             //Social media block at the bottom of the responsive menu
             $('<div id="mobile-social"><ul>' +
                 '<li class="linkedin"><a href="https://www.linkedin.com/in/llalihassanll" target="_blank">LinkedIn</a></li>' +
